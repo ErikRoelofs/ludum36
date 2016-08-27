b56f80bb5171734a2514e87a88a9d56969713dd3
Erik Roelofs <erik@ruigekonijnen.nl>
2016-08-27 08:21:07 +0200

    Hammer mouse

--- main.lua
+++ main.lua
@@ -1,13 +1,30 @@
 love.load = function()
+  love.mouse.setVisible(false)
+  
+  hammer = {
+    image = love.graphics.newImage("images/hammer.png"),
+    x = 0,
+    y = 0
+  }
+  
   looky = require "looky"
   
-  root = looky:build("root")
+  root = looky:build("stackroot")
+  
+  hammerPane = looky:build("dragbox", {width = "fill", height="fill"})
+  hammerView = looky:build("image", {width="wrap", height="wrap", file=hammer.image})
+  
+  root:addChild(hammerPane)
+  hammerPane:addChild(hammerView)
+  
   
   root:layoutingPass()
 end
 
 love.update = function(dt)
-  
+  hammer.x, hammer.y = love.mouse.getPosition()
+  hammerPane.offset = {hammer.x - hammer.image:getWidth()  / 2, hammer.y - hammer.image:getHeight()}
+  root:update(dt)
 end
 
 love.draw = function()
