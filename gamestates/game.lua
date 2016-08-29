9056d995e136d7fbdbb126e87eb4a770c85980f9
Erik Roelofs <eroelofs@zigwebsoftware.nl>
2016-08-29 13:59:28 +0200

    Moved most of the mainfile to a state file

--- gamestates/game.lua
+++ gamestates/game.lua
@@ -1,11 +1,63 @@
 return function()
   return {
     load = function(self)
-    
-    end,
-    update = function(self)
+      rows = 4
+      columns = 4
+      computers = {}
       
+      for i = 1, rows do
+        for j = 1, columns do
+          local faction = "bad"
+          if love.math.random(0,1) == 1 then
+            faction = "good"
+          end
+          table.insert(computers, newComputer("(" .. i .. "," .. j .. ")", faction))
+        end
+      end
+
+      getComputer = function(i,j)
+        return computers[i + ((j-1) * columns)]
+      end
+      
+      victory = require "gamestates/game/victory"()  
+      victory:load()
+
+      renderOvermap = function(self)
+        love.graphics.setColor(255,255,255,255)    
+        love.graphics.draw(hammerModel.image, hammerModel.x, hammerModel.y, hammerModel.r, 2, 2, hammerModel.image:getWidth() / 2, hammerModel.image:getHeight())
+        
+        for _, j in ipairs(junkItems) do
+          love.graphics.draw(j.image, j.x, j.y, j.r, 1.5, 1.5, j.image:getWidth()/2, j.image:getHeight()/2)
+        end
+        
+        for _, smoke in ipairs(smokeItems) do
+          love.graphics.draw(smoke.ps, smoke.x, smoke.y)
+        end
+        
+      end
+
+      root = layout()
+
+      
+      junk.load()
+      smoke.load()
     end,
-    
+    update = function(self, dt)
+      junk.update(dt)
+      hammer:update(dt)
+      smoke.update(dt)
+      
+      for i = 1, rows * columns do 
+        computers[i]:update(dt)
+      end
+      
+      victory:update(dt)
+    end,
+    mousepressed = function(self, x, y, button)
+      hammer.mousepressed(x,y,button)
+    end,
+    draw = function(self)
+      root:render()
+    end
   }
 end
--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@ if debug then require("mobdebug").start() end
 
 love.load = function()
   
+  -- initial load of static data
   require "computer/computerModel"
   require "computer/behaviors"
   
@@ -11,8 +12,6 @@ love.load = function()
   hammer.load()
   smoke = require "gamestates/game/smoke"
   
-  love.mouse.setVisible(true)
-
   images = {
     monitor = love.graphics.newImage("images/monitor.png"),
     smoke = love.graphics.newImage("images/junk/smoke.png")
@@ -29,72 +28,24 @@ love.load = function()
     off = love.graphics.newImage("images/faces/off.png"),
   }
   
-  rows = 4
-  columns = 4
-  computers = {}
-  
-  for i = 1, rows do
-    for j = 1, columns do
-      local faction = "bad"
-      if love.math.random(0,1) == 1 then
-        faction = "good"
-      end
-      table.insert(computers, newComputer("(" .. i .. "," .. j .. ")", faction))
-    end
-  end
-
-  getComputer = function(i,j)
-    return computers[i + ((j-1) * columns)]
-  end
-  
-  victory = require "gamestates/game/victory"()  
-  victory:load()
-
-  renderOvermap = function(self)
-    love.graphics.setColor(255,255,255,255)    
-    love.graphics.draw(hammerModel.image, hammerModel.x, hammerModel.y, hammerModel.r, 2, 2, hammerModel.image:getWidth() / 2, hammerModel.image:getHeight())
-    
-    for _, j in ipairs(junkItems) do
-      love.graphics.draw(j.image, j.x, j.y, j.r, 1.5, 1.5, j.image:getWidth()/2, j.image:getHeight()/2)
-    end
-    
-    for _, smoke in ipairs(smokeItems) do
-      love.graphics.draw(smoke.ps, smoke.x, smoke.y)
-    end
-    
-  end
-
-  
   looky = require "looky"
   ease = require "easy"  
-  root = require "baselayout"
+  layout = require "baselayout"
+
+  -- specific load for a game
+  state = require "gamestates/game"()
+  state.load()
   
-  junk.load()
-  smoke.load()
-  
-  root:layoutingPass()
-  
- 
 end
 
 love.update = function(dt)
- 
-  junk.update(dt)
-  hammer:update(dt)
-  smoke.update(dt)
-  
-  for i = 1, rows * columns do 
-    computers[i]:update(dt)
-  end
-  
-  victory:update(dt)
+ state:update(dt)
 end
 
 love.mousepressed = function(x,y,button)
-  hammer.mousepressed(x,y,button)
+  state:mousepressed(x,y,button)
 end
 
 love.draw = function()
-  root:render()
-  
+  state:draw()  
 end
